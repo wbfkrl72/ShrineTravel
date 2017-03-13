e3b058bc864d5c591df3c7d116eb7ce4538609e6
wbfkrl72 <wbfkrl72@gmail.com>
2017-03-13 15:30:10 +0900

    Initial commit

---
+++ app/src/main/java/wbfkrl/good/shrinetravel/MainActivity.java
@@ -0,0 +1,328 @@
+package wbfkrl.good.shrinetravel;
+
+import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private RelativeLayout mainLayout;
+    private GoogleMap mMap;
+
+    private CompassView mCompassView;
+    private SensorManager mSensorManager;
+    private boolean mCompassEnabled;
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        setUpMap();
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(37.566535,126.9779692);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Seoul"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    public void setUpMap(){
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+        /*
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        boolean sideBottom = true;
+        mCompassView = new CompassView(this);
+        mCompassView.setVisibility(View.VISIBLE);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        params.addRule(sideBottom ? RelativeLayout.ALIGN_PARENT_BOTTOM : RelativeLayout.ALIGN_PARENT_TOP);
+
+        mainLayout.addView(mCompassView, params);
+
+
+        mCompassEnabled = true;
+
+        // 위치 확인하여 위치 표시 시작
+        startLocationService();
+
+        checkDangerousPermissions();
+        */
+    }
+
+
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 있음", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "권한 설명 필요함.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, 1);
+            }
+        }
+    }
+
+    //For Search
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            Log.d("test", latLng.toString());
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
+    public void onNormalMap(View view) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    public void onSatelliteMap(View view) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
+    public void onTerrainMap(View view) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+    }
+
+
+    public void onHybridMap(View view) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    }
+
+
+
+
+
+
+/*
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인됨.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        try {
+            // 내 위치 자동 표시 enable
+            map.setMyLocationEnabled(true);
+
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+        if(mCompassEnabled) {
+            mSensorManager.registerListener(mListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            // 내 위치 자동 표시 disable
+            map.setMyLocationEnabled(false);
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+        if(mCompassEnabled) {
+            mSensorManager.unregisterListener(mListener);
+        }
+    }
+
+    private void startLocationService() {
+        // 위치 관리자 객체 참조
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // 리스너 객체 생성
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+        try {
+            // GPS 기반 위치 요청
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+
+            // 네트워크 기반 위치 요청
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(getApplicationContext(), "위치 확인 시작함. 로그를 확인하세요.", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private class GPSListener implements LocationListener {
+
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String msg = "Latitude : "+ latitude + "\nLongitude:"+ longitude;
+            Log.i("GPSLocationService", msg);
+
+            // 현재 위치의 지도를 보여주기 위해 정의한 메소드 호출
+            showCurrentLocation(latitude, longitude);
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+    }
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+        // 현재 위치를 이용해 LatLon 객체 생성
+        LatLng curPoint = new LatLng(latitude, longitude);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        // 지도 유형 설정. 지형도인 경우에는 GoogleMap.MAP_TYPE_TERRAIN, 위성 지도인 경우에는 GoogleMap.MAP_TYPE_SATELLITE
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // 현재 위치 주위에 아이콘을 표시하기 위해 정의한 메소드
+        showAllBankItems(latitude, longitude);
+    }
+
+
+    private void showAllBankItems(Double latitude, Double longitude) {
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(new LatLng(latitude+0.001, longitude+0.001));
+        marker.title("● 지점명 : \n 국민은행(낙성대지점)\n");
+        marker.snippet("● 주소 : \n 서울시 관악구 낙성대동");
+        marker.draggable(true);
+        marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.bank));
+
+        map.addMarker(marker);
+    }
+
+
+    private final SensorEventListener mListener = new SensorEventListener() {
+        private int iOrientation = -1;
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+        // 센서의 값을 받을 수 있도록 호출되는 메소드
+        public void onSensorChanged(SensorEvent event) {
+            if (iOrientation < 0) {
+                iOrientation = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+            }
+
+            mCompassView.setAzimuth(event.values[0] + 90 * iOrientation);
+            mCompassView.invalidate();
+
+        }
+
+    };
+
+    */
+
+}
+
+
